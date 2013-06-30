44a07cb7c7b74654df5349c54252622cc8e75f2a
Grant Byrne <grant@grantbyrne.com>
2013-06-30 15:14:59 -0400

    Added basic domain objects to support logging.
    In lieu of using tools like Log4Net or Emlah, I've decided to start
    writing my own logging framework. We'll see how this goes.

---
+++ DndDev.Domain/Logging/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DndDev.Domain.Logging
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Message { get; set; }
+        public string Exception { get; set; }
+    }
+}
---
+++ DndDev.Domain/Logging/Severity.cs
@@ -0,0 +1,56 @@
+﻿namespace DndDev.Domain.Logging
+{
+    /// <summary>
+    /// Explicates the severity of a log entry
+    /// </summary>
+    /// <remarks>
+    /// Can also be used to throttle the amount of logging to the database, 
+    /// though this feature is currently not supported
+    /// </remarks>
+    public enum Severity
+    {
+        /// <summary>
+        /// The highest possible rank and is intended to turn off logging.
+        /// </summary>
+        Off,
+
+        /// <summary>
+        /// Severe errors that cause premature termination. Expect these 
+        /// to be immediately visible on a status console.
+        /// </summary>
+        Fatal,
+
+        /// <summary>
+        /// Other runtime errors or unexpected conditions. Expect these 
+        /// to be immediately visible on a status console.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// Use of deprecated APIs, poor use of API, 'almost' errors, 
+        /// other runtime situations that are undesirable or unexpected, 
+        /// but not necessarily "wrong". Expect these to be immediately 
+        /// visible on a status console.
+        /// </summary>
+        Warn,
+
+        /// <summary>
+        ///  Interesting runtime events (startup/shutdown). Expect these 
+        /// to be immediately visible on a console, so be conservative 
+        /// and keep to a minimum. 
+        /// </summary>
+        Info,
+
+        /// <summary>
+        /// Detailed information on the flow through the system. Expect 
+        /// these to be written to logs only.
+        /// </summary>
+        Debug,
+
+        /// <summary>
+        /// Most detailed information. Expect these to be written to logs 
+        /// only.
+        /// </summary>
+        Trace
+    }
+}
