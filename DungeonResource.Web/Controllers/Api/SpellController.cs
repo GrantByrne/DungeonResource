75a472878bc08c7df260b8fe02d6028da8221ca9
holymoo <grant@grantbyrne.com>
2013-11-02 20:10:59 -0400

    Syncing up versions

---
+++ DungeonResource.Web/Controllers/Api/SpellController.cs
@@ -0,0 +1,45 @@
+﻿using DungeonResource.Components.Domain.Spell;
+using DungeonResource.Components.Service.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DungeonResource.Web.Controllers.Api
+{
+    /// <summary>
+    /// Handles restful spell operations
+    /// </summary>
+    public class SpellController : ApiController
+    {
+        /// <summary>
+        /// Handles the management of the spell data
+        /// </summary>
+        private readonly ISpellService _spellService;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="spellService"></param>
+        public SpellController(ISpellService spellService)
+        {
+            _spellService = spellService;
+        }
+        
+        /// <summary>
+        /// Reads a spell out of the database
+        /// </summary>
+        /// <param name="id">
+        /// The UID for the spell
+        /// </param>
+        /// <returns>
+        /// The data associated with a single spell
+        /// </returns>
+        public Spell Get(int id)
+        {
+            return _spellService.ReadSpell(id);
+        }
+    }
+}
--- DungeonResource.Web/Scripts/App/Controllers/UpdateSpellController.js
+++ DungeonResource.Web/Scripts/App/Controllers/UpdateSpellController.js
@@ -1,1 +1,61 @@
 ﻿
+myApp.controller('UpdateSpellController', ['$scope', '$log', '$http', '$location', function ($scope, $log, $http, $location) {
+
+    $scope.addSpellLevel = function () {
+
+        var spellLevel = {
+            SpellClass: $scope.spellClass,
+            Level: $scope.spellLevel
+        };
+
+        // Add the class to the 
+        $scope.spell.SpellLevels.push(spellLevel);
+
+        // Clear out the data on those textboxes
+        $scope.spellClass = undefined;
+        $scope.spellLevel = undefined;
+
+    };
+
+    $scope.remove = function (array, index) {
+        array.splice(index, 1);
+    };
+
+    $scope.readSpell = function() {
+
+        // Get the id of the item from the query string
+        var id = 129;
+
+        var config = {
+            method: 'GET',
+            url: '/api/Get?id=' + id
+        };
+
+        $http(config).
+          success(function (data) {
+              $scope.spell = data;
+          });
+
+    };
+
+    $scope.updateSpell = function () {
+
+        var config = {
+            method: 'POST',
+            url: 'Update',
+            data: $scope.spell
+        };
+
+        $http(config).
+          success(function (data) {
+
+              // Move the user back to the homepage
+              window.location = '/';
+
+          });
+
+    };
+
+    $scope.saveError = false;
+
+}]);
